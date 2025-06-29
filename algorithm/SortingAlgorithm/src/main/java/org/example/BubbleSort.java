426d92c5edf6149a1dcdbe764dc849c21117a93d
Miyuru Bashitha <miyurubashitha100@gmail.com>
2025-06-30 00:06:02 +0530

    add bubble function

--- algorithm/SortingAlgorithm/src/main/java/org/example/BubbleSort.java
+++ algorithm/SortingAlgorithm/src/main/java/org/example/BubbleSort.java
@@ -5,24 +5,35 @@ public class BubbleSort {
     public static void main(String[] args){
 
         int[] arr = {3,2,6,4,5,1};
-        bubbleSort(arr);
+        bubbleSortAscendingOrder(arr);
         for (int i : arr) {
             System.out.print(i);
         }
     }
 
-    public static void bubbleSort(int[] arr){
+    public static void bubbleSortAscendingOrder(int[] arr){
 
         for(int i = 0; i < arr.length-1 ; i++){
-            for(int j = 0; j <arr.length-1;j++){
-                if(arr[i]>arr[j]){
-                    int temp = arr[j];
-                    arr[j] = arr[i];
-                    arr[i]=temp;
+            for(int j = 0; j < arr.length-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j]=temp;
                 }
             }
         }
 
+    }
+    public static void bubbleSortDescendingOrder(int[] arr){
+        for(int i = 0; i < arr.length-1 ; i++){
+            for(int j = 0; j < arr.length-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
     }
 
 }
