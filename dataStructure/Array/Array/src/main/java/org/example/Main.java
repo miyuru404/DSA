4ef90ca29ea59b79ba6a80fd48a21f6fea0e0dfa
Miyuru Bashitha <miyurubashitha100@gmail.com>
2025-06-29 12:34:50 +0530

    array project: add print array function

--- dataStructure/Array/Array/src/main/java/org/example/Main.java
+++ dataStructure/Array/Array/src/main/java/org/example/Main.java
@@ -2,6 +2,22 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int[] arr = {2,5,8,1,3,4,7,6,9,};
+
+
+
     }
+}
+
+class ArrayOperation{
+
+    public static void printArray(int[] arr){
+        for(int num:arr){
+            System.out.print(num);
+        }
+
+    }
+
+
 }
