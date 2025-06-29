0e7a2b4a157d9f31fdeef402908839a901e941a7
Miyuru Bashitha <miyurubashitha100@gmail.com>
2025-06-29 23:53:04 +0530

    add bubble function

--- algorithm/SortingAlgorithm/src/main/java/org/example/BubbleSort.java
+++ algorithm/SortingAlgorithm/src/main/java/org/example/BubbleSort.java
@@ -2,7 +2,26 @@ package org.example;
 
 public class BubbleSort {
 
+    public static void main(String[] args){
+
+        int[] arr = {3,2,6,4,5,1};
+        bubbleSort(arr);
+        for (int i : arr) {
+            System.out.print(i);
+        }
+    }
+
     public static void bubbleSort(int[] arr){
+
+        for(int i = 0; i < arr.length-1 ; i++){
+            for(int j = 0; j <arr.length-1;j++){
+                if(arr[i]>arr[j]){
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i]=temp;
+                }
+            }
+        }
 
     }
 
