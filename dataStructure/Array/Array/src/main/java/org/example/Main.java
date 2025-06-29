425f099e1de76f43d286e83bbc8ffb3707db902a
Miyuru Bashitha <miyurubashitha100@gmail.com>
2025-06-29 12:37:43 +0530

    array project: add addValueToEnd function

--- dataStructure/Array/Array/src/main/java/org/example/Main.java
+++ dataStructure/Array/Array/src/main/java/org/example/Main.java
@@ -27,6 +27,7 @@ class ArrayOperation{
         arr[index] = value;
         printArray(arr);
     }
+
     public static void deleteValueByIndex(int[] arr , int index){
         for(int i = index ; i<arr.length-1 ;i++){
             arr[i] = arr[i+1];
@@ -34,6 +35,33 @@ class ArrayOperation{
         arr[arr.length-1] = 0;
         printArray(arr);
     }
+
+    public static int[] addValueToEnd(int[] arr , int value){
+
+        //creating a new array with old array size + 1
+        int[] newArray = new int[arr.length+1];
+
+        // add old arrays value to new arry
+        for(int i=0; i<arr.length;i++){
+            newArray[i] = arr[i];
+        }
+        // add new value at end of new array
+        newArray[newArray.length-1] = value;
+
+        System.out.print("old array: ");
+        printArray(arr);
+        System.out.println();
+        System.out.print("new array: ");
+        printArray(newArray);
+
+        return newArray;
+
+
+    }
+
+    public static int[]addValueAtFirst(){return null;}
+
+    public static int[] addValueAtMiddle(){return null;}
 
 
 
